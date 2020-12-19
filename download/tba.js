0092840ecc7c5a31895120ea97de67a1d857a7df
tba2020 <mike.tbacity@gmail.com>
2020-12-19 11:54:16 +0900

    impove detacting

--- download/tba.js
+++ download/tba.js
@@ -1,15 +1,20 @@
 jQuery(function($) {
   const is_mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-  setTimeout(function(){
-    // hide addthis mobile buttons on pc
-    if(is_mobile){
-      // true for mobile device
-      $('.at-share-btn.at-svc-kakaotalk, .at-share-btn.at-svc-sms').show();
-      $('.at-share-btn.at-svc-kakao').hide();
+  const hide_mobile_share_button = function(){
+    if($('.at-share-btn').length) {
+      // hide addthis mobile buttons on pc
+      if(is_mobile){
+        // true for mobile device
+        $('.at-share-btn.at-svc-kakaotalk, .at-share-btn.at-svc-sms').show();
+        $('.at-share-btn.at-svc-kakao').hide();
+      } else {
+        // false for not mobile device
+        $('.at-share-btn.at-svc-kakaotalk, .at-share-btn.at-svc-sms').hide();
+        $('.at-share-btn.at-svc-kakao').show();
+      }
     } else {
-      // false for not mobile device
-      $('.at-share-btn.at-svc-kakaotalk, .at-share-btn.at-svc-sms').hide();
-      $('.at-share-btn.at-svc-kakao').show();
+      setTimeout(hide_mobile_share_button,100);
     }
-  },1000);
+  };
+  setTimeout(hide_mobile_share_button,300);
 })
